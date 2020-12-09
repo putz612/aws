e5cf11f7496fded575bb995591aa39fbfe24fd4b
Jason Sievert <jason.sievert@serko.com>
2020-12-09 10:32:23 -0600

    Creating public-private blocks

--- vpc/subnet.tf
+++ vpc/subnet.tf
@@ -1,10 +1,21 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   count                   = length(data.aws_availability_zones.available.names)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet("10.0.0.0/16", 4, count.index)
   map_public_ip_on_launch = true
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   tags = {
-    "Name" = "main-${element(data.aws_availability_zones.available.names, count.index)}"
+    "Name" = "public-${element(data.aws_availability_zones.available.names, count.index)}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(data.aws_availability_zones.available.names)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 4, count.index+length(data.aws_availability_zones.available.names))
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    "Name" = "private-${element(data.aws_availability_zones.available.names, count.index)}"
   }
 }
