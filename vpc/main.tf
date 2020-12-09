7f0dbb65a49052af4d0315b960a7b5036191f006
Jason Sievert <jason.sievert@serko.com>
2020-12-09 09:56:07 -0600

    Cleaning names up and removing subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,14 +15,7 @@ resource "aws_vpc" "main" {
 
 }
 
-resource "aws_subnet" "prod-subnet-public-1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-west-2a"
-}
-
-resource "aws_internet_gateway" "prod-igw" {
+resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.main.id
 }
 
@@ -30,12 +23,8 @@ resource "aws_route_table" "prod-public-crt" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.prod-igw.id
+    gateway_id = aws_internet_gateway.gateway.id
   }
 }
 
-resource "aws_route_table_association" "prod-crta-public" {
-  subnet_id      = aws_subnet.prod-subnet-public-1.id
-  route_table_id = aws_route_table.prod-public-crt.id
-}
 
