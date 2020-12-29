bd04148ef13f4795955cdf19e3c33611de92c99f
Jason Sievert <jason.sievert@serko.com>
2020-12-29 16:47:21 -0600

    Format cleaning

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,10 +1,11 @@
 variable "AWS_REGION" {
   description = "What region are we going to use"
-  default = "us-west-2"
+  default     = "us-west-2"
 }
+
 variable "az" {
   description = "List of az we are going to use"
-  type = list(tuple([string]))
+  type        = list(tuple([string]))
   default = [
     ["a"],
     ["b"],
