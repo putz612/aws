f0d776c0c317cf2249efc986c6ba7eb2655ae8a0
Jason Sievert <jason.sievert@serko.com>
2020-12-29 16:48:16 -0600

    Using remote state storage

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.AWS_REGION
 }
+
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "cyberglitch-terraform-state"
+    region         = "us-west-2"
+    key            = "terraform/dev/terraform.tfstate"
+    dynamodb_table = "terraform_state"
+  }
+}
