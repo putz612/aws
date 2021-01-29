bfcde0e7fcedd35cd129db0aa96ce02bdd3bd27f
Jason Sievert <jason.sievert@serko.com>
2021-01-29 15:10:45 -0600

    Fixing path and adding trigger

--- vpc/ami.tf
+++ vpc/ami.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "build_custom_ami" {
   triggers = {
     aws_ami_id      = data.aws_ami.ubuntu.id
-    #sha256_userdata = filesha256("deploy/packer-customize.sh")
+    sha256_userdata = filesha256("../packer/baseInit.sh")
   }
  
   provisioner "local-exec" {
@@ -14,10 +14,10 @@ resource "null_resource" "build_custom_ami" {
     set -ex;
     packer validate \
       -var "aws_region=$VAR_AWS_REGION" \
-      packer-configs/custom_ami.json
+     ../packer/ubuntu-focal.json
     packer build \
       -var "aws_region=$VAR_AWS_REGION" \
-      packer-configs/custom_ami.json
+      ../packer/ubuntu-focal.json
 EOF
   }
 }
